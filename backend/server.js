1bcc9e9d3236f9959255062b67a2ba0d191db425
DESIKA S <desikas209@gmail.com>
2026-05-19 13:31:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  process.env.FRONTEND_URL,
+  process.env.https://stsm-eight.vercel.app/,
 ].filter(Boolean);
 app.use(cors({ origin: allowedOrigins, credentials: true }));   
 app.use(express.json({ limit: "10mb" }));
