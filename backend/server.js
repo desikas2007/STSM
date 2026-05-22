26d693fef03c40a925452976e62c89e0bfd36e0f
DESIKA S <desikas209@gmail.com>
2026-05-22 22:21:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: ["http://localhost:3000", "http://localhost:5173","https://stsm-eight.vercel.app"],
     credentials: true,
   })
 );
